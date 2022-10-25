e139445997dedd9070ea0c537d4ab90a5254a2bf
Safar Safarov <Safarov.safar@hotmail.com>
2022-10-25 22:47:28 +0500

    ✨

---
+++ Cursus/Cursus/CursusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursusApp.swift
+//  Cursus
+//
+//  Created by Safar Safarov on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CursusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
