dd434c9cc64d0b341be3ddbf3a516b4d2429a0bb
Safar Safarov <Safarov.safar@hotmail.com>
2022-10-28 00:02:06 +0500

    ✨

--- Cursus/Cursus/Views/OnboardingView.swift
+++ Cursus/Cursus/Views/OnboardingView.swift
@@ -15,23 +15,42 @@ struct OnboardingView: View {
         ZStack {
             background
             
-            button.view()
-                .frame(width: 236, height: 64)
-                .overlay(
-                    Label("Start the course", systemImage: "arrow.forward")
-                        .offset(x: 4, y: 4)
-                        .font(.headline)
-                )
-                .background(
-                    Color.black
-                        .cornerRadius(30)
-                        .blur(radius: 10)
-                        .opacity(0.3)
-                        .offset(y: 10)
-                )
-                .onTapGesture {
-                    button.play(animationName: "active")
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Learn design & code")
+                    .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
+                    .frame(width: 260, alignment: .leading)
+                
+                Text("Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.")
+                    .customFont(.body)
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+                
+                button.view()
+                    .frame(width: 236, height: 64)
+                    .overlay(
+                        Label("Start the course", systemImage: "arrow.forward")
+                            .offset(x: 4, y: 4)
+                            .font(.headline)
+                    )
+                    .background(
+                        Color.black
+                            .cornerRadius(30)
+                            .blur(radius: 10)
+                            .opacity(0.3)
+                            .offset(y: 10)
+                    )
+                    .onTapGesture {
+                        button.play(animationName: "active")
                 }
+                
+                Text("Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.")
+                    .customFont(.footnote)
+                    .opacity(0.7)
+            }
+            .padding(40)
+            .padding(.top, 40)
         }
     }
     
@@ -52,4 +71,3 @@ struct OnboardingView_Previews: PreviewProvider {
         OnboardingView()
     }
 }
-
