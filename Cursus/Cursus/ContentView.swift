a70393a1de081bc1e72c2b825d6a76b17418c0b6
Safar Safarov <Safarov.safar@hotmail.com>
2022-11-04 23:13:53 +0500

    ✨

--- Cursus/Cursus/ContentView.swift
+++ Cursus/Cursus/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
+    @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    @State var isOpen = false
+    let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false, animationName: "open")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            switch selectedTab {
+            case .chat:
+                Text("Chat")
+            case .search:
+                Text("Search")
+            case .timer:
+                Text("Timer")
+            case .bell:
+                Text("Bell")
+            case .user:
+                Text("User")
+            }
+            
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color("Shadow").opacity(0.2), radius: 5, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    try? button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
+            
+            TabBar()
         }
-        .padding()
     }
 }
 
